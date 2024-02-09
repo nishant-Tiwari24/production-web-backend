976b7993056d60b6764f63451925e66500462242
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-02-09 10:25:17 +0530

    todo data modelling

--- data-modelling-mongoose/models/todos/sub_todo.models.js
+++ data-modelling-mongoose/models/todos/sub_todo.models.js
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
 
-const subtodoSchema = new mongoose.Schema({},{timestamps:true})
+const subtodoSchema = new mongoose.Schema({
+    content : {
+        type: String,
+        required: true
+    },
+    complete : {
+        type: Boolean,
+        default: false
+    },
+    createdBy : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+    }
+},{timestamps:true})
 
 export const SubTodo = mongoose.model("SubTodo",subtodoSchema)
--- data-modelling-mongoose/models/todos/todos.model.js
+++ data-modelling-mongoose/models/todos/todos.model.js
@@ -5,7 +5,20 @@ const todoSchema = new mongoose.Schema({
         type:String,
         required:true,
     },
-    
+    complete:{
+        type:Boolean,
+        required:true,
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    subtodos: [
+        {
+            type: mongoose.Schema.Types.ObjectId,  
+            ref: "Subtodo",
+        }
+    ] //Array of subtodos
 },{timestamps:true});
 
 export const Todo = mongoose.model("Todo", todoSchema);
