242226c009132b7a0146636a8a0ed68f5118d114
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-02-17 00:52:52 +0530

    app js directory

--- professional-backend-setup/src/app.js
+++ professional-backend-setup/src/app.js
@@ -1,5 +1,14 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({limit: "16kb "}))
 
 export { app }
