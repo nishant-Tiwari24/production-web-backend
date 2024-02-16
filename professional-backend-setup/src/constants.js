ef3fab6df1857b6931f36926312a25c9fc0ed154
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-02-16 23:19:25 +0530

    api response

--- professional-backend-setup/src/constants.js
+++ professional-backend-setup/src/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = 'youtube'
+export const DB_NAME = 'youtube' //application database name
--- professional-backend-setup/src/index.js
+++ professional-backend-setup/src/index.js
@@ -1,22 +1,33 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-const app = express();
+dotenv.config({
+    path:'./env'
+})
 
-;( async() => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error",()=>{
-            console.log("ERROR: ",error);
-            throw error;
-        })
-        app.listen(process.env.PORT,() => {
-            console.log(`Server is running on port ${process.env.PORT}`);
-        })
+// const app = express();
 
-    } catch (error) {
-        console.log("ERROR: ",error);
-        throw error;
-    }
-})()
+// ;( async() => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",()=>{
+//             console.log("ERROR: ",error);
+//             throw error;
+//         })
+//         app.listen(process.env.PORT,() => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.log("ERROR: ",error);
+//         throw error;
+//     }
+// })()
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err)=>console.log(err))
