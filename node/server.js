0a6921e0845450fb6940f87c6fb6692233e3704e
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-04-27 13:16:07 +0530

    express

--- node/server.js
+++ node/server.js
@@ -5,6 +5,12 @@ const data = JSON.parse(fs.readFileSync('data.json','utf-8'))
 import express from 'express'
 const app = express();
 
+app.use((req,res,next) => {
+    console.log(req.get('User-Agent') , req.body);
+    next();
+
+})
+app.get('/',(req,res) => res.send(data))
 app.listen(3000,() => {console.log("Server Started")})
 
 
